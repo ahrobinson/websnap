42feb29f8f6ad5d69cb2f1c3530cd106e8d4e35f
Alon Robinson <ahrobins3@gmail.com>
2016-03-08 22:55:09 -0800

    add image upload

--- app/app.js
+++ app/app.js
@@ -1,13 +1,37 @@
 var socket = io();
 
+function previewFile () {
+  var preview = $('img')[0];
+  var file    = $('#files')[0].files[0];
+  var reader  = new FileReader();
+
+  reader.onload = function () {
+    sendChat(reader.result);
+  }
+
+  if (file) {
+    reader.readAsDataURL(file);
+  }
+}
+
+function sendChat (val) {
+  socket.emit('chat msg', val);
+}
+
 $('form').on('submit', function () {
-  socket.emit('chat msg', $('#m').val());
+  sendChat($('#m').val());
   $('#m').val('');
   return false;
 });
 
+
 socket.on('chat msg', function (msg) {
-  $('#messages').append($('<li>').text(msg.body).attr('id', msg._id));
+  if(msg.body.match('image')){
+    console.log(msg)
+    $('#messages').append($('<li>').html('<img src="'+msg+'"/>'));
+  } else {
+    $('#messages').append($('<li>').text(msg.body).attr('id', msg._id));
+  }
   setTimeout(function () {
     socket.emit('deletion', msg._id);
   }, 5000);
--- server/sockets.js
+++ server/sockets.js
@@ -4,7 +4,8 @@ module.exports = function (server) {
 
   var io = require('socket.io')(server);
 
-  io.on('connection', function (socket) {
+  io.sockets.on('connection', function (socket) {
+    // console.log(socket)
     console.log('a user connected');
     socket.on('chat msg', function (msg) {
       controller.create(msg, function (message) {
