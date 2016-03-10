8945367d1efa932759e9ba9b748821af52071abb
Alon Robinson <ahrobins3@gmail.com>
2016-03-10 15:57:17 -0800

    add webRTC video

--- app/app.js
+++ app/app.js
@@ -1,5 +1,44 @@
 var socket = io();
 
+
+
+function video (vid) {
+  console.log('vid: ', vid);
+  var video = $('video')[0];
+  video.src = window.URL.createObjectURL(vid);
+  video.onloadedmetadata = function(e) {
+    // Do something with the video here.
+    console.log(video)
+  };
+}
+
+$('#vidBtn').click(function () {
+  navigator.webkitGetUserMedia({
+    'audio': true,
+    'video': true
+  },video,
+  function (err) {
+    console.log('Error: ', err);
+  });
+});
+
+
+
+
+
+
+
+
+/*
+*
+*
+*
+*       SEND CHAT MSG
+*
+*
+*
+*/
+
 function previewFile () {
   var preview = $('img')[0];
   var file    = $('#files')[0].files[0];
@@ -18,6 +57,7 @@ function sendChat (val) {
   socket.emit('chat msg', val);
 }
 
+
 $('form').on('submit', function () {
   sendChat($('#m').val());
   $('#m').val('');
@@ -28,7 +68,7 @@ $('form').on('submit', function () {
 socket.on('chat msg', function (msg) {
   if(msg.body.match('image')){
     console.log(msg)
-    $('#messages').append($('<li>').html('<img src="'+msg+'"/>'));
+    $('#messages').append($('<li>').html('<img src="'+msg.body+'"/>').attr('id', msg._id));
   } else {
     $('#messages').append($('<li>').text(msg.body).attr('id', msg._id));
   }
